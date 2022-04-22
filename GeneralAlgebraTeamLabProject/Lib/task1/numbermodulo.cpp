f3239dbfaa43475f00d7c9deaadb1f05d6b95f94
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-04-22 15:07:34 +0300

    task1: first commit

---
+++ GeneralAlgebraTeamLabProject/Lib/task1/numbermodulo.cpp
@@ -0,0 +1,6 @@
+#include "numbermodulo.h"
+
+NumberModulo::NumberModulo()
+{
+
+}
---
+++ GeneralAlgebraTeamLabProject/Lib/task1/numbermodulo.h
@@ -0,0 +1,12 @@
+#ifndef NUMBERMODULO_H
+#define NUMBERMODULO_H
+
+#include "Lib_global.h"
+
+class LIB_EXPORT NumberModulo
+{
+public:
+    NumberModulo();
+};
+
+#endif // NUMBERMODULO_H
