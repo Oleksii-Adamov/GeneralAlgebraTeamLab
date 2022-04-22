a8d1ae3344651386e2d303e728886b6fc5920ad0
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-04-22 17:47:35 +0300

    task1: Added set_modulo get_modulo and test for them

--- GeneralAlgebraTeamLabProject/Lib/task1/numbermodulo.cpp
+++ GeneralAlgebraTeamLabProject/Lib/task1/numbermodulo.cpp
@@ -14,3 +14,11 @@ long long NumberModulo::get_num()
 {
     return num;
 }
+
+void NumberModulo::set_modulo(long long input) {
+    if (input < 1) throw "Modulo is less than 1";
+    modulo = input;
+}
+long long NumberModulo::get_modulo() {
+    return modulo;
+}
--- GeneralAlgebraTeamLabProject/TestNumberModulo/tst_testnumbermodulo.cpp
+++ GeneralAlgebraTeamLabProject/TestNumberModulo/tst_testnumbermodulo.cpp
@@ -12,6 +12,9 @@ public:
     ~TestNumberModulo();
 
 private slots:
+    void test_case_get_modulo();
+    void test_case_set_modulo_positive();
+    void test_case_set_modulo_non_positive();
     void test_case_get_num();
     void test_case_set_num();
 
@@ -27,7 +30,7 @@ TestNumberModulo::~TestNumberModulo()
 
 }
 
-// tests test_case_get_num and test_case_set_num are depend on each other
+// get and set tests depend on each other. I don't want to use friend for this.
 
 void TestNumberModulo::test_case_get_num()
 {
@@ -47,6 +50,27 @@ void TestNumberModulo::test_case_set_num()
     // to do negative (maybe should in different test_case
 }
 
+void TestNumberModulo::test_case_get_modulo()
+{
+    NumberModulo test_num;
+    QCOMPARE(test_num.get_modulo(), 2);
+    test_num.set_modulo(7);
+    QCOMPARE(test_num.get_modulo(), 7);
+}
+void TestNumberModulo::test_case_set_modulo_positive()
+{
+    NumberModulo test_num;
+    test_num.set_modulo(5);
+    QCOMPARE(test_num.get_modulo(), 5);
+    test_num.set_modulo(3);
+    QCOMPARE(test_num.get_modulo(), 3);
+}
+void TestNumberModulo::test_case_set_modulo_non_positive()
+{
+    NumberModulo test_num;
+    QVERIFY_THROWS_EXCEPTION( char*, test_num.set_modulo(0));
+    QVERIFY_THROWS_EXCEPTION( char*, test_num.set_modulo(-2));
+}
 
 QTEST_APPLESS_MAIN(TestNumberModulo)
 
