dab83a4c7587b39559eab024944f524e8aac0248
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-04-24 14:12:36 +0300

    Merge test 2

--- GeneralAlgebraTeamLabProject/TestNumberModulo/tst_testnumbermodulo.cpp
+++ GeneralAlgebraTeamLabProject/TestNumberModulo/tst_testnumbermodulo.cpp
@@ -68,7 +68,7 @@ void TestNumberModulo::test_case_mod()
     IntModulo test_num;
     // comment for merge test
     QVERIFY_THROWS_EXCEPTION(std::invalid_argument, test_num.mod(0));
-    // postive modulo
+    // merge test
     // positive num
     test_num.set_num(5);
     test_num.mod(2);
