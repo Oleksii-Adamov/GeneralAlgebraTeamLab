c3dc50dcf5eeae780bcda077dce9f3e0d2a47f17
leirimnad <leirimnad.web@gmail.com>
2022-05-19 17:48:35 +0200

    differenciate prime and non-prime p

--- GeneralAlgebraTeamLabProject/Lib/task6/squareroot.cpp
+++ GeneralAlgebraTeamLabProject/Lib/task6/squareroot.cpp
@@ -5,9 +5,47 @@ SquareRoot::SquareRoot()
 
 }
 
+
+
 // finding a square root of a, modulo p
 std::pair<long long, long long> SquareRoot::sqrt(long long a, long long p)
 {
-    // just for a test
+    if (isPrime(p))
+        return sqrtPrimeP(a, p);
+    else
+        return sqrtNonPrimeP(a, p);
+}
+
+
+bool SquareRoot::isPrime(long long n)
+{
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (long long i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+
+
+// for prime p
+std::pair<long long, long long> SquareRoot::sqrtPrimeP(long long a, long long p)
+{
     return std::make_pair(7ll, -7ll);
 }
+
+// for non prime p
+std::pair<long long, long long> SquareRoot::sqrtNonPrimeP(long long a, long long p)
+{
+    return std::make_pair(7ll, -7ll);
+}
+
+
