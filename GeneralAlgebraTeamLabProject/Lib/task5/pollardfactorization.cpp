ca788798ac04029bb05f23eaa53db39b6c2191b9
Mark Yavorskyi <yavorskiymark@gmail.com>
2022-05-23 17:58:57 +0300

    Fixed: algo crushes on 12

--- GeneralAlgebraTeamLabProject/Lib/task5/pollardfactorization.cpp
+++ GeneralAlgebraTeamLabProject/Lib/task5/pollardfactorization.cpp
@@ -1,34 +1,68 @@
 #include "pollardfactorization.h"
 #include <stdlib.h>
 #include <algorithm>
+#include <cmath>
+#include <stdexcept>
 
 // finds greatest common divider
 static int gcd(int a, int b) {
     return b == 0 ? a : gcd (b, a % b);
 }
 
-static int rhoPollard (int N) {
-    int x = rand() % (N-3) + 1;
-    int y = 1, i = 0, stage = 2;
-    while (gcd(N, abs(x - y)) == 1)
-    {
-        if (i == stage){
-            y = x;
-            stage = stage*2;
-        }
-        x = (x * x + 1) % N;
-        i = i + 1;
+static bool isPrime(int a) {
+    for (int i = 2; i <= sqrt(a); ++i) {
+        if (a % i == 0) return false;
     }
-    return gcd(N, abs(x-y));
+    return true;
+}
+
+static int F(int x, int a, int mod) {
+    return (x * x + (1 + 2 * a)) % mod;
+}
+
+static std::vector<int> rhoPollard(int N) {
+    if (N < 1) {
+        throw std::invalid_argument(N + " is less than 1");
+    }
+    if (N == 1) {
+        return std::vector<int>{1};
+    }
+    if (N % 2 == 0) {
+        std::vector<int> t = rhoPollard(N / 2);
+        t.push_back(2);
+        return t;
+    }
+    std::vector<int> ans;
+    int x = 2;
+    int y = 2;
+    int gcd1;
+    int a = rand() % 10;
+    do {
+        x = F(x, a, N);
+        y = F(F(y, a, N), a, N);
+        gcd1 = gcd(N, abs(x - y));
+    } while (gcd1 == 1);
+
+    if (isPrime(gcd1)) {
+        ans.push_back(gcd1);
+    } else {
+        std::vector<int>  t = rhoPollard(gcd1);
+        ans.insert(ans.end(), t.begin(), t.end());
+    }
+
+    int zvor = N / gcd1;
+    if (isPrime(zvor)) {
+        ans.push_back(zvor);
+    } else {
+        std::vector<int>  t = rhoPollard(zvor);
+        ans.insert(ans.end(), t.begin(), t.end());
+    }
+
+    return ans;
 }
 
 std::vector<int> PollardFactorization::factorize(int number) {
-    std::vector<int> primeNumbers;
-    while (number != 1) {
-        int n = rhoPollard(number);
-        primeNumbers.push_back(n);
-        number /= n;
-    }
+    std::vector<int> primeNumbers = rhoPollard(number);
     sort(primeNumbers.begin(), primeNumbers.end());
     return primeNumbers;
 }
--- GeneralAlgebraTeamLabProject/TestFactorization/tst_testfactorization.cpp
+++ GeneralAlgebraTeamLabProject/TestFactorization/tst_testfactorization.cpp
@@ -14,6 +14,7 @@ public:
 
 private slots:
     void test_case1();
+    void test_case2();
 
 };
 
@@ -29,17 +30,30 @@ TestFactorization::~TestFactorization()
 
 void TestFactorization::test_case1()
 {
-    std::vector<int> pn1 = PollardFactorization().factorize(91);
+    std::vector<int> pn1 = PollardFactorization::factorize(91);
     std::vector<int> ans1{7, 13};
     QCOMPARE(ans1, pn1);
 
-    std::vector<int> pn2 = PollardFactorization().factorize(4642135);
+    std::vector<int> pn2 = PollardFactorization::factorize(4642135);
     std::vector<int> ans2{5, 928427};
     QCOMPARE(ans2, pn2);
 
-    std::vector<int> pn3 = PollardFactorization().factorize(4641);
+    std::vector<int> pn3 = PollardFactorization::factorize(4641);
     std::vector<int> ans3{3, 7, 13, 17};
     QCOMPARE(ans3, pn3);
+}
+
+void TestFactorization::test_case2()
+{
+    for (int i = 2; i <= 100000; ++i) {
+        bool isExceptionThrown = false;
+        try {
+            std::vector<int> pn = PollardFactorization::factorize(i);
+        } catch(std::exception e) {
+            isExceptionThrown = true;
+        }
+        QVERIFY(!isExceptionThrown);
+    }
 }
 
 QTEST_APPLESS_MAIN(TestFactorization)
