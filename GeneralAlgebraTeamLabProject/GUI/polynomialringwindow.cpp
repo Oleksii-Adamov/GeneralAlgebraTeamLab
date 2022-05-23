3a3be2fa09d353829921182e537f8c13135bb556
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-05-23 15:42:49 +0300

    FiniteFieldWindow: Trying validation

--- GeneralAlgebraTeamLabProject/GUI/polynomialringwindow.cpp
+++ GeneralAlgebraTeamLabProject/GUI/polynomialringwindow.cpp
@@ -8,11 +8,11 @@ PolynomialRingWindow::PolynomialRingWindow(QWidget *parent) :
     ui(new Ui::PolynomialRingWindow)
 {
     ui->setupUi(this);
-    /*QRegularExpression rx("^[0-9]+$");
-    QValidator* validator = new QRegularExpressionValidator(rx, this);
-    ui->lineEdit_modulus->setValidator(validator);
-    ui->lineEdit_first->setValidator(validator);
-    ui->lineEdit_second->setValidator(validator);*/
+    //QRegularExpression rx(/*"^[0-9x ]+$"*/"/[0-9]*[ ]*[x]+[ ]*[+]?[ ]*/");
+    //QValidator* validator = new QRegularExpressionValidator(rx, this);
+    //ui->lineEdit_modulus->setValidator(validator);
+    //ui->lineEdit_first->setValidator(validator);
+    //ui->lineEdit_second->setValidator(validator);
 }
 
 PolynomialRingWindow::~PolynomialRingWindow()
@@ -24,10 +24,15 @@ void PolynomialRingWindow::on_pushButton_add_clicked()
 {
 
     unsigned long long modulus;
-    Polinome first(ui->lineEdit_first->text().toStdString());
-    Polinome second(ui->lineEdit_second->text().toStdString());
-    modulus = std::stoull(ui->lineEdit_modulus->text().toStdString());
-    Polinome ans = first.add(second, modulus);
-    ui->lineEdit_ans->setText(QString::fromStdString(ans.toString()));
+    try {
+        Polinome first(ui->lineEdit_first->text().toStdString());
+        Polinome second(ui->lineEdit_second->text().toStdString());
+        modulus = std::stoull(ui->lineEdit_modulus->text().toStdString());
+        Polinome ans = first.add(second, modulus);
+        ui->lineEdit_ans->setText(QString::fromStdString(ans.toString()));
+    }
+    catch(std::exception& e) {
+        ui->lineEdit_ans->setText(e.what());
+    }
 }
 
