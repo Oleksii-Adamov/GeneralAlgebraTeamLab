cf53cc76734f5ade1e6ff0d799c40ecee1d38c8a
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-05-23 11:49:23 +0300

    FiniteFieldWindow: implemented multiply, divide, reverse, Pollard factorization

--- GeneralAlgebraTeamLabProject/GUI/finitefieldwindow.cpp
+++ GeneralAlgebraTeamLabProject/GUI/finitefieldwindow.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <QRegularExpressionValidator>
 #include "task1/intmodulo.h"
-
+#include "task5/pollardfactorization.h"
 
 FiniteFieldWindow::FiniteFieldWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -40,13 +40,17 @@ void FiniteFieldWindow::set_ans(const QString& ans) {
     ui->lineEdit_ans->setText(ans);
 }
 
+void FiniteFieldWindow::set_ans(IntModulo& ans) {
+    ui->lineEdit_ans->setText(QString::number(ans.get_num()));
+}
+
 void FiniteFieldWindow::on_pushButton_add_clicked()
 {
     IntModulo first, second;
     unsigned long long modulus;
     read_and_mod(first, second, modulus);
     first.add(second, modulus);
-    set_ans(QString::number(first.get_num()));
+    set_ans(first);
 }
 
 
@@ -56,6 +60,71 @@ void FiniteFieldWindow::on_pushButton_substract_clicked()
     unsigned long long modulus;
     read_and_mod(first, second, modulus);
     first.subtract(second, modulus);
-    set_ans(QString::number(first.get_num()));
+    set_ans(first);
+}
+
+
+void FiniteFieldWindow::on_pushButton_multiply_clicked()
+{
+    IntModulo first, second;
+    unsigned long long modulus;
+    read_and_mod(first, second, modulus);
+    first.multiply(second, modulus);
+    set_ans(first);
+}
+
+
+void FiniteFieldWindow::on_pushButton_pow_clicked()
+{
+    IntModulo first;
+    unsigned long long modulus, exponent = std::stoull(ui->lineEdit_second->text().toStdString());
+    read_and_mod(first, modulus);
+    first.pow(exponent, modulus);
+    set_ans(first);
+}
+
+
+void FiniteFieldWindow::on_pushButton_divide_clicked()
+{
+    IntModulo first, second;
+    unsigned long long modulus;
+    read_and_mod(first, second, modulus);
+    try {
+        first.divide(second, modulus);
+        set_ans(first);
+    }
+    catch(std::logic_error& e) {
+        set_ans("Дільник немає оберненого");
+    }
+}
+
+
+void FiniteFieldWindow::on_pushButton_reverse_clicked()
+{
+    IntModulo first;
+    unsigned long long modulus;
+    read_and_mod(first, modulus);
+    IntModulo ans = first.findReversed(modulus);
+    if (ans.get_num() == 0) {
+        set_ans("Елемент немає оберненого");
+    }
+    else {
+        set_ans(ans);
+    }
+}
+
+
+void FiniteFieldWindow::on_pushButton_factorization_Pollard_clicked()
+{
+    IntModulo first;
+    unsigned long long modulus;
+    read_and_mod(first, modulus);
+    std::vector<int> vec_ans = PollardFactorization::factorize(first.get_num());
+    QString ans = "";
+    for (std::size_t i = 0; i < vec_ans.size(); i++) {
+        ans += QString::number(vec_ans[i]);
+        if (i != vec_ans.size() - 1) ans += "*";
+    }
+    set_ans(ans);
 }
 
--- GeneralAlgebraTeamLabProject/GUI/finitefieldwindow.h
+++ GeneralAlgebraTeamLabProject/GUI/finitefieldwindow.h
@@ -21,11 +21,21 @@ private slots:
 
     void on_pushButton_substract_clicked();
 
+    void on_pushButton_multiply_clicked();
+
+    void on_pushButton_pow_clicked();
+
+    void on_pushButton_divide_clicked();
+
+    void on_pushButton_reverse_clicked();
+
+    void on_pushButton_factorization_Pollard_clicked();
+
 private:
     void read_and_mod(IntModulo& first, IntModulo& second, unsigned long long& modulus);
     void read_and_mod(IntModulo& first, unsigned long long& modulus);
     void set_ans(const QString& ans);
-
+    void set_ans(IntModulo& ans);
     Ui::FiniteFieldWindow *ui;
 };
 
