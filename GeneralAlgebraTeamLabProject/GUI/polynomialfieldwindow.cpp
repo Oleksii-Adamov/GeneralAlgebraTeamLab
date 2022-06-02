54c511d80c92437c66169687ce7b5a69bbc5e06e
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-06-02 23:23:23 +0300

    PolynomialFieldWindow: fixed pow

--- GeneralAlgebraTeamLabProject/GUI/polynomialfieldwindow.cpp
+++ GeneralAlgebraTeamLabProject/GUI/polynomialfieldwindow.cpp
@@ -156,7 +156,7 @@ void PolynomialFieldWindow::on_pushButton_pow_clicked()
         // possible change for valid_ll if negatives will be added
         if (!valid_ull(window->ui->lineEdit_second->text())) throw std::invalid_argument("Друге поле введено неправильно");
         long long exponent = std::stoll(window->ui->lineEdit_second->text().toStdString());
-        Polinome ans = first.pow(exponent, /*irreducible,*/ modulus);
+        Polinome ans = first.pow(exponent, irreducible, modulus);
         window->set_ans(ans);
     }, this);
 }
